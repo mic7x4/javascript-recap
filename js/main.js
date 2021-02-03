565e15a68626fbd272ce13d5b27d55f5c624c4f5
mic7x4 <mic7x4@gmail.com>
2021-02-03 12:33:54 +0200

    first -commit

---
+++ js/main.js
@@ -0,0 +1,88 @@
+let name = 'Crook';
+let age = 40;
+
+// Concatenation
+// const names = "Ndabaga Michel";
+// const fruits = 'Tuna, Blackberry, Berry';
+// const newFruits = fruits.split(', ');
+// const fruited =  newFruits.map((fruit)=>console.log(fruit))
+// console.log(fruits.split(', '));
+
+// Arrays
+// const fruits = ['tuna', 'apple', 'pie'];
+// fruits[3] = 'Avocadoes'; 
+// fruits.push('Homo');
+// fruits.unshift('No homo');
+// console.log(Array.isArray(fruits));
+// fruits.map((fruit)=>console.log(fruit));
+
+// Object
+// const person = {
+//     firstname:'Crook',
+//     lastname:'artwork',
+//     age:30,
+//     hobbies:['games', 'music','sports'],
+//     address:{
+//         street:'KN 370 kk',
+//         city: 'Boston',
+//         state:'MA'
+//     }
+// }
+// person.hobbies.map((hobby)=> console.log(hobby))
+
+// Array of objects
+const todos =[
+    {
+        id:1,
+        text:'Go to the doc',
+        isCompleted:false
+    },
+    {
+        id:2,
+        text:'Clean the house',
+        isCompleted:true
+    },
+    {
+        id:3,
+        text:'Date with cougar',
+        isCompleted:false
+    },
+    {
+        id:4,
+        text:'Date with cougar',
+        isCompleted:true
+    },
+]
+// todos.map((todo) => console.log(todo.id));
+// todos.forEach((todo) => console.log(todo.text));
+
+// const todoText = todos.map((todo) =>  todo.text);
+// console.log(todoText);
+
+// const todoCompleted = todos.filter((todo) =>{
+//   return   todo.isCompleted === true
+// }).map((todo)=>todo.text)
+// console.log(todoCompleted);
+
+// Conditions if , tenary
+
+// const x= 7;
+// const color = x > 10 ? 'red' : 'blue';
+
+// switch(color){
+//     case 'red':
+//         console.log('Color is Red');
+//         break;
+//     case 'blue':
+//         console.log('The fucking color is Blue');
+//         break;
+//     default:
+//         console.log('Unknown Color');
+//         break;
+// }
+
+// const number = (num1,num2)=>{
+//     return num1 + num2;
+// }
+
+// 
