d144bfa9b43d7985632ca03fefcbeac0bfaaf0af
mic7x4 <mic7x4@gmail.com>
2021-02-04 02:36:25 +0200

    fetching data from json placeholder

--- js/fetchApi.js
+++ js/fetchApi.js
@@ -2,6 +2,8 @@ console.log('It works!');
 
 document.getElementById('getText').addEventListener('click',getText);
 document.getElementById('getUsers').addEventListener('click',getUsers);
+document.getElementById('getPosts').addEventListener('click',getPosts);
+document.getElementById('addPost').addEventListener('submit',addPost);
 
 
 function getText(){
@@ -24,4 +26,38 @@ function getUsers(){
         });
         document.getElementById('output').innerHTML = output;
     })
+}
+
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res)=>res.json())
+    .then((data) => {
+        let output = '<h2>Posts</h2>';
+        data.forEach((post) => {
+            output += `
+                <div>
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                </div>
+            `
+        })
+        document.getElementById('output').innerHTML = output;
+    })
+}
+
+function addPost(e){
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let body =  document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method:'POST',
+        headers:{
+            'Accept':'application/json,text/plain,*/*',
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({title,body})
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
 }
